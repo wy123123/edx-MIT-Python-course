5d5bc032e827f167e1a8dfadd12ffe4dcb50e816
wwwyyuu <wangyu0917@hotmail.com>
2015-09-29 00:01:03 +0800

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -59,3 +59,21 @@ def uniqueValues(aDict):
         if count == 1:
             l = l+[i]
     return sorted(l)
+
+def satisfiesF(L):
+
+   count= len(L)
+   rec_list=list()
+
+   for i in range(0,count):
+     if(f(L[i])==False):
+        rec_list.append(L[i])   
+
+   if(rec_list):
+     for j in rec_list:
+      L.remove(j)
+
+
+
+   return len(L)
+run_satisfiesF(L, satisfiesF) 
